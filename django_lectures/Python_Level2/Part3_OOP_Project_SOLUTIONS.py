ae8cf4ba9c4b41e1c37ab7a05f592977e7e3f470
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-06-18 13:32:36 -0500

    completed war game:

--- django_lectures/Python_Level2/Part3_OOP_Project_SOLUTIONS.py
+++ django_lectures/Python_Level2/Part3_OOP_Project_SOLUTIONS.py
@@ -36,77 +36,62 @@ class Deck:
     """
     This is the Deck Class. This object will create a deck of cards to initiate
     play. You can then use this Deck list of cards to split in half and give to
-    the players. It will use SUITE and RANKS to create the deck. It should also
-    have a method for splitting/cutting the deck in half and Shuffling the deck.
+    the players.
     """
-    # Creates a giant list of all the cards in the deck
     def __init__(self):
-        print("Creating new ordered deck!")
-        self.all_cards = [(s, r) for s in SUITE for r in RANKS]
+        print("Creating New Ordered Deck")
+        self.allcards = [(s,r) for s in SUITE for r in RANKS ]
 
-    # Randomizes the order of the cards
     def shuffle(self):
-        print("Shuffling the deck...")
-        shuffle(self.all_cards)
-    
-    # Split the deck into two halves stored in a tuple
-    # Tuple unpacking
+        print("Shuffling Deck")
+        shuffle(self.allcards)
+
     def split_in_half(self):
-        return (self.all_cards[:26], self.all_cards[26:])
+        return (self.allcards[:26],self.allcards[26:])
 
 class Hand:
     '''
-    This is the Hand class. Each player has a Hand, and can add or remove
-    cards from that hand. There should be an add and remove card method here.
+    This is the Hand class. Each player has a hand, and can add or remove
+    cards from that hand.
     '''
-    def __init__(self, cards):
+    def __init__(self,cards):
         self.cards = cards
-    
-    # allows us to see how many cards are in a hand
+
     def __str__(self):
         return "Contains {} cards".format(len(self.cards))
-    
-    # allows us to a dd a card to our hand
-    def add(self, added_cards):
+
+    def add(self,added_cards):
         self.cards.extend(added_cards)
 
-    # allows us to remove a card from the deck
     def remove_card(self):
         return self.cards.pop()
 
 class Player:
-    """
-    This is the Player class, which takes in a name and an instance of a Hand
-    class object. The Payer can then play cards and check if they still have cards.
-    """
-    def __init__(self, name, hand):
+
+    def __init__(self,name,hand):
         self.name = name
         self.hand = hand
-    
-    # allows a player to remove cards from their hand so that they can play them
+
     def play_card(self):
         drawn_card = self.hand.remove_card()
-        print("{} has placed: {}".format(self.name, drawn_card))
-        # prints whitespace
-        print("\n")
+        print("{} has placed: {}".format(self.name,drawn_card))
+        print('\n')
         return drawn_card
 
-    # if two cards are the same, this will allow the player to initiate war
     def remove_war_cards(self):
         war_cards = []
         if len(self.hand.cards) < 3:
-            return self.hand.cards
+            return war_cards
         else:
             for x in range(3):
                 war_cards.append(self.hand.cards.pop())
             return war_cards
-    
+
     def still_has_cards(self):
         """
-        Return True if player still has cards left
+        Returns True if player still has cards
         """
         return len(self.hand.cards) != 0
-
 
 
 ######################
@@ -114,63 +99,72 @@ class Player:
 ######################
 print("Welcome to War, let's begin...")
 
-# Use the 3 classes along with some logic to play a game of war!
-
-# Create a new deck and split it in half:
+# Create New Deck and split in half
 d = Deck()
 d.shuffle()
-half1, half2 = d.split_in_half()
+half1,half2 = d.split_in_half()
 
-# create both players!
+# Create Both Players
+comp = Player("computer",Hand(half1))
+name = input("What is your name player? ")
+user = Player(name,Hand(half2))
 
-comp = Player("Computer", Hand(half1))
-
-name = input("What is your name?")
-
-user = Player(name, Hand(half2))
-
+# Set Round Count
 total_rounds = 0
 war_count = 0
-
+# Let's play
 while user.still_has_cards() and comp.still_has_cards():
     total_rounds += 1
-    print("Time for a new round!")
-    print("Here are the current standings")
-    print(user.name + "has the count: " + str(len(user.hand.cards)))
-    print(comp.name + "has the count: " + str(len(comp.hand.cards)))
-    print("Play a card!")
+    print("It is time for a new round!")
+    print("Here are the current standings: ")
+    print(user.name+" count: "+str(len(user.hand.cards)))
+    print(comp.name+" count: "+str(len(comp.hand.cards)))
+    print("Both players play a card!")
     print('\n')
 
+    # Cards on Table represented by list
     table_cards = []
 
+    # Play cards
     c_card = comp.play_card()
     p_card = user.play_card()
 
+    # Add to table_cards
     table_cards.append(c_card)
     table_cards.append(p_card)
 
+    # Check for War!
     if c_card[1] == p_card[1]:
-        war_count += 1
-
-        print("WAR!")
-
+        war_count +=1
+        print("We have a match, time for war!")
+        print("Each player removes 3 cards 'face down' and then one card face up")
         table_cards.extend(user.remove_war_cards())
         table_cards.extend(comp.remove_war_cards())
 
+        # Play cards
+        c_card = comp.play_card()
+        p_card = user.play_card()
+
+        # Add to table_cards
+        table_cards.append(c_card)
+        table_cards.append(p_card)
+
+        # Check to see who had higher rank
         if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
+            print(user.name+" has the higher card, adding to hand.")
             user.hand.add(table_cards)
         else:
+            print(comp.name+" has the higher card, adding to hand.")
             comp.hand.add(table_cards)
 
     else:
+        # Check to see who had higher rank
         if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
+            print(user.name+" has the higher card, adding to hand.")
             user.hand.add(table_cards)
         else:
+            print(comp.name+" has the higher card, adding to hand.")
             comp.hand.add(table_cards)
 
-print("Game over, number of rounds: " + str(total_rounds))
-print("a war happend " + str(war_count) + " times")
-print("Does the computer still have cards?")
-print(str(comp.still_has_cards()))
-print("Does the human user still have cards?")
-print(str(user.still_has_cards()))
+print("Great Game, it lasted: "+str(total_rounds))
+print("A war occured "+str(war_count)+" times.")
